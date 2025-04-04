81d5190bdbfcaae02dc38460459bf4bcff305e2b
satwik1111 <naikn5637@gmail.com>
2025-04-04 11:08:28 +0530

    save file

---
+++ Parent.java
@@ -0,0 +1,26 @@
+class Parent {
+    String name;
+
+    Parent(String name) {
+        this.name = name;
+    }
+    void showname() {
+        System.out.println("name of Parent: " + name);
+    }
+}
+class Child extends Parent {
+    Child(String name) {
+        super(name);
+    }
+    void showChildname() {
+        System.out.println("name of Child: " + name);
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Child child = new Child("john);
+        child.showname(); 
+        child.showChildname();
+    }
+}
